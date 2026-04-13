538f534aa3cb84b4ae3ad328c9b024e9bf824ad7
Mika Aßmus <mika.assmus@gmail.com>
2026-04-13 21:48:53 +0200

    Simplefied binding power system by removing unnecessary complexity everywhere.

--- src/tokenizing/binding_pow.rs
+++ src/tokenizing/binding_pow.rs
@@ -3,34 +3,16 @@ use crate::tokenizing::token::{Bracket, Token, TokenKind};
 
 use TokenKind::*;
 
-pub const ALL: u8 = 0;
-pub const STMT: u8 = 1;
-pub const EXPR: u8 = 2;
-
-pub const WRITE: u8 = 10;
-pub const WRITE_RIGHT: u8 = 11;
-
 pub const LABEL: u8 = 15; // allows tick
 
-pub const OR: u8 = 20;
-pub const OR_RIGHT: u8 = 21;
-pub const XOR: u8 = 30;
-pub const XOR_RIGHT: u8 = 31;
-pub const AND: u8 = 40;
-pub const AND_RIGHT: u8 = 41;
+pub const BOOLEAN: u8 = 20;
+pub const BOOLEAN_RIGHT: u8 = 21;
 
 pub const COMPARISON: u8 = 50;
 pub const COMPARISON_RIGHT: u8 = 51;
 
-pub const BIT_SHIFT: u8 = 60;
-pub const BIT_SHIFT_RIGHT: u8 = 61;
-
-pub const BIT_OR: u8 = 70;
-pub const BIT_OR_RIGHT: u8 = 71;
-pub const BIT_XOR: u8 = 80;
-pub const BIT_XOR_RIGHT: u8 = 81;
-pub const BIT_AND: u8 = 90;
-pub const BIT_AND_RIGHT: u8 = 91;
+pub const BITWISE: u8 = 60;
+pub const BITWISE_RIGHT: u8 = 61;
 
 pub const ADDITIVE: u8 = 100;
 pub const ADDITIVE_RIGHT: u8 = 101;
@@ -38,9 +20,8 @@ pub const ADDITIVE_RIGHT: u8 = 101;
 pub const MULTIPLICATIVE: u8 = 110;
 pub const MULTIPLICATIVE_RIGHT: u8 = 111;
 
-pub const NEGATION_RIGHT: u8 = 114;
+pub const UNARY_PREFIX: u8 = 130;
 
-pub const PREFIX: u8 = 130;
 pub const APPLICATION: u8 = 140;
 
 pub const CALL_CONVENTION: u8 = 150;
@@ -65,9 +46,8 @@ impl<'src> Token<'src> {
             | Type
             | Open(Bracket::Curly)
             | Semicolon
-            | Colon => 0,
-
-            ColonColon
+            | Colon
+            | ColonColon
             | Equal
             | LeftLeftEqual
             | RightRightEqual
@@ -91,22 +71,25 @@ impl<'src> Token<'src> {
             | DotEqual
             | CrossEqual
             | PlusPlus
-            | DashDash => WRITE,
+            | DashDash => 0,
 
             Tick => LABEL,
 
-            PipePipe | NotPipePipe => OR,
-            RightPipePipe | NotRightPipePipe => XOR,
-            AndAnd | NotAndAnd => AND,
+            PipePipe | NotPipePipe | RightPipePipe | NotRightPipePipe | AndAnd | NotAndAnd => {
+                BOOLEAN
+            }
 
             EqualEqual | NotEqual | Left | NotLeft | LeftEqual | NotLeftEqual | Right
             | NotRight | RightEqual | NotRightEqual => COMPARISON,
 
-            LeftLeft | RightRight => BIT_SHIFT,
-
-            Pipe | NotPipe => BIT_OR,
-            RightPipe | NotRightPipe => BIT_XOR,
-            TokenKind::And | NotAnd => BIT_AND,
+            LeftLeft
+            | RightRight
+            | Pipe
+            | NotPipe
+            | RightPipe
+            | NotRightPipe
+            | TokenKind::And
+            | NotAnd => BITWISE,
 
             Plus | Dash => ADDITIVE,
 
@@ -125,10 +108,10 @@ impl UnaryOp {
         match self {
             Deref => panic!("These don't have right binding power!"),
 
-            Neg => NEGATION_RIGHT,
-            Not => PREFIX,
+            Neg => UNARY_PREFIX,
+            Not => UNARY_PREFIX,
 
-            Ptr => PREFIX,
+            Ptr => UNARY_PREFIX,
         }
     }
 }
@@ -140,17 +123,18 @@ impl BinaryOp {
         match self {
             Index | App => panic!("These dont have right binding power!"),
 
-            BinaryOp::Or | Nor => OR_RIGHT,
-            BinaryOp::Xor | Xnor => XOR_RIGHT,
-            BinaryOp::And | Nand => AND_RIGHT,
+            BinaryOp::Or | Nor | BinaryOp::Xor | Xnor | BinaryOp::And | Nand => BOOLEAN_RIGHT,
 
             Eq | Ne | Less | LessEq | Greater | GreaterEq => COMPARISON_RIGHT,
 
-            Lsh | Rsh => BIT_SHIFT_RIGHT,
-
-            BinaryOp::BitOr | BitNor => BIT_OR_RIGHT,
-            BinaryOp::BitXor | BitXnor => BIT_XOR_RIGHT,
-            BinaryOp::BitAnd | BitNand => BIT_AND_RIGHT,
+            Lsh
+            | Rsh
+            | BinaryOp::BitOr
+            | BitNor
+            | BinaryOp::BitXor
+            | BitXnor
+            | BinaryOp::BitAnd
+            | BitNand => BITWISE_RIGHT,
 
             Add | Sub => ADDITIVE_RIGHT,
 
