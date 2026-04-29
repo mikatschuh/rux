2fb87b1065ee8239361a372270b30df01a0a0b45
Mika Aßmus <mika.assmus@gmail.com>
2026-04-29 14:42:45 +0200

    Removed all code that had to do with the loop phi variants because thats not needed anymore.

--- src/grapher/graph.rs
+++ src/grapher/graph.rs
@@ -10,19 +10,14 @@ use crate::{
 };
 
 pub type DataID<'src> = Rc<DataNode<'src>, NoDealloc>;
-pub type CtrlID<'src> = Rc<CtrlNode<'src>, NoDealloc>;
+pub type CtrlID<'src> = Rc<CtrlKind<'src>, NoDealloc>;
 pub type BranchID<'src> = Rc<Branch<'src>, NoDealloc>;
 pub type MergeID<'src> = Rc<Merge<'src>, NoDealloc>;
-pub type LoopID<'src> = Rc<Loop<'src>, NoDealloc>;
+pub type PhiID<'src> = Rc<Phi<'src>, NoDealloc>;
 
 #[derive(Debug, PartialEq, Eq, Clone, Hash)]
 pub struct DataNode<'src> {
     pub kind: DataKind<'src>,
-}
-
-#[derive(Debug, PartialEq, Eq, Clone, Hash)]
-pub struct CtrlNode<'src> {
-    pub kind: CtrlKind<'src>,
 }
 
 #[derive(Debug, PartialEq, Eq, Clone, Hash)]
@@ -32,38 +27,22 @@ pub struct Branch<'src> {
 }
 
 #[derive(Debug, PartialEq, Eq, Clone, Hash)]
-pub struct Loop<'src> {
-    pub ctrl: CtrlID<'src>,
-    pub condition: DataID<'src>,
-    pub backedge: CtrlID<'src>,
-}
-
-#[derive(Debug, PartialEq, Eq, Clone, Hash)]
 pub struct Merge<'src> {
     pub branches: Vec<CtrlID<'src>>, // classically: false - true
 }
 
 #[derive(Debug, PartialEq, Eq, Clone, Hash)]
+pub struct Phi<'src> {
+    pub merge: MergeID<'src>,
+    pub variants: Vec<DataID<'src>>,
+}
+
+#[derive(Debug, PartialEq, Eq, Clone, Hash)]
 pub enum CtrlKind<'src> {
     Start,
-    Merge {
-        merge: MergeID<'src>,
-    },
-    TrueBranch {
-        branch: BranchID<'src>,
-    },
-    FalseBranch {
-        branch: BranchID<'src>,
-    },
-
-    LoopHead {
-        ctrl: CtrlID<'src>,
-        condition: DataID<'src>,
-        backedge: CtrlID<'src>,
-    },
-    PlaceHolder {
-        ctrl: CtrlID<'src>,
-    },
+    Merge { merge: MergeID<'src> },
+    TrueBranch { branch: BranchID<'src> },
+    FalseBranch { branch: BranchID<'src> },
 }
 
 #[derive(Debug, PartialEq, Eq, Clone, Hash)]
@@ -79,6 +58,7 @@ pub enum DataKind<'src> {
     },
     Boolean(bool),
     Unit,
+    Never,
 
     Unary {
         op: UnaryOp,
@@ -94,15 +74,8 @@ pub enum DataKind<'src> {
         addr: DataID<'src>,
     },
 
-    BranchPhi {
-        merge: MergeID<'src>,
-        when_true: DataID<'src>,
-        when_false: DataID<'src>,
-    },
-    LoopPhi {
-        loop_head: LoopID<'src>,
-        entry: DataID<'src>,
-        backedge: DataID<'src>,
+    Phi {
+        phi: PhiID<'src>,
     },
 
     Unknown,
@@ -121,6 +94,7 @@ enum DataKey<'src> {
     },
     Boolean(bool),
     Unit,
+    Never,
     Unary {
         op: UnaryOp,
         input: usize,
@@ -134,15 +108,9 @@ enum DataKey<'src> {
         mem: usize,
         addr: usize,
     },
-    BranchPhi {
+    Phi {
         merge: usize,
-        when_true: usize,
-        when_false: usize,
-    },
-    LoopPhi {
-        loop_head: usize,
-        entry: usize,
-        backedge: usize,
+        variants: Vec<usize>,
     },
 }
 
@@ -150,7 +118,7 @@ enum DataKey<'src> {
 pub struct Graph<'src> {
     arena: Bump,
     node_cache: HashMap<DataKey<'src>, DataID<'src>>,
-    pub current_mem: CtrlID<'src>,
+    pub current_ctrl: CtrlID<'src>,
 }
 impl<'src> Graph<'src> {
     pub fn new() -> Self {
@@ -158,12 +126,7 @@ impl<'src> Graph<'src> {
 
         Self {
             node_cache: HashMap::new(),
-            current_mem: Rc::<CtrlNode<'src>, NoDealloc>::new_in_bump(
-                CtrlNode {
-                    kind: CtrlKind::Start,
-                },
-                &arena,
-            ),
+            current_ctrl: Rc::<CtrlKind<'src>, NoDealloc>::new_in_bump(CtrlKind::Start, &arena),
             arena,
         }
     }
@@ -188,9 +151,8 @@ impl<'src> Graph<'src> {
         Rc::<DataNode<'src>, NoDealloc>::new_in_bump(DataNode { kind }, &self.arena)
     }
 
-    fn push_mem_node(&mut self, kind: CtrlKind<'src>) -> CtrlID<'src> {
-        let node: CtrlNode<'src> = CtrlNode { kind };
-        Rc::<CtrlNode<'src>, NoDealloc>::new_in_bump(node, &self.arena)
+    fn push_ctrl_node(&mut self, node: CtrlKind<'src>) -> CtrlID<'src> {
+        Rc::<CtrlKind<'src>, NoDealloc>::new_in_bump(node, &self.arena)
     }
 
     fn push_branch(&mut self, branch: Branch<'src>) -> BranchID<'src> {
@@ -202,37 +164,23 @@ impl<'src> Graph<'src> {
     }
 
     pub fn add_ctrl_merge(&mut self, merge: MergeID<'src>) -> CtrlID<'src> {
-        self.push_mem_node(CtrlKind::Merge { merge })
+        self.push_ctrl_node(CtrlKind::Merge { merge })
     }
 
     pub fn add_branch(&mut self, condition: DataID<'src>) -> (CtrlID<'src>, CtrlID<'src>) {
-        let ctrl = self.current_mem();
+        let ctrl = self.current_ctrl();
         let branch = self.push_branch(Branch { ctrl, condition });
         (
-            self.push_mem_node(CtrlKind::FalseBranch {
+            self.push_ctrl_node(CtrlKind::FalseBranch {
                 branch: branch.clone(),
             }),
-            self.push_mem_node(CtrlKind::TrueBranch { branch }),
+            self.push_ctrl_node(CtrlKind::TrueBranch { branch }),
         )
-    }
-
-    pub fn add_loop_head(&mut self, condition: DataID<'src>) -> CtrlID<'src> {
-        let ctrl = self.current_mem();
-        let place_holder = self.add_placeholder(ctrl.clone());
-        self.push_mem_node(CtrlKind::LoopHead {
-            condition,
-            ctrl,
-            backedge: self.current_mem(),
-        })
-    }
-
-    pub fn add_placeholder(&mut self, ctrl: CtrlID<'src>) -> CtrlID<'src> {
-        self.push_mem_node(CtrlKind::PlaceHolder { ctrl })
     }
 
     pub fn add_load(&mut self, addr: DataID<'src>) -> DataID<'src> {
         self.push_node(DataKind::Load {
-            mem: self.current_mem(),
+            mem: self.current_ctrl(),
             addr,
         })
     }
@@ -254,17 +202,16 @@ impl<'src> Graph<'src> {
         self.push_node(DataKind::Binary { op, lhs, rhs })
     }
 
-    pub fn add_phi(
-        &mut self,
-        merge: MergeID<'src>,
-        when_true: DataID<'src>,
-        when_false: DataID<'src>,
-    ) -> DataID<'src> {
-        self.push_node(DataKind::BranchPhi {
-            merge,
-            when_true,
-            when_false,
-        })
+    pub fn add_phi(&mut self, merge: MergeID<'src>, variants: Vec<DataID<'src>>) -> PhiID<'src> {
+        Rc::<Phi<'src>, NoDealloc>::new_in_bump(Phi { merge, variants }, &self.arena)
+    }
+
+    pub fn add_data_phi(&mut self, phi: PhiID<'src>) -> DataID<'src> {
+        self.push_node(DataKind::Phi { phi })
+    }
+
+    pub fn add_phi_no_dedup(&mut self, phi: PhiID<'src>) -> DataID<'src> {
+        self.push_node_no_dedup(DataKind::Phi { phi })
     }
 
     pub fn add_quote(&mut self, quote: String) -> DataID<'src> {
@@ -287,8 +234,12 @@ impl<'src> Graph<'src> {
         self.push_node(DataKind::Unit)
     }
 
-    pub fn current_mem(&self) -> CtrlID<'src> {
-        self.current_mem.clone()
+    pub fn add_never(&mut self) -> DataID<'src> {
+        self.push_node(DataKind::Never)
+    }
+
+    pub fn current_ctrl(&self) -> CtrlID<'src> {
+        self.current_ctrl.clone()
     }
 }
 
@@ -304,6 +255,7 @@ impl<'src> DataKind<'src> {
             }),
             DataKind::Boolean(boolean) => Some(DataKey::Boolean(*boolean)),
             DataKind::Unit => Some(DataKey::Unit),
+            DataKind::Never => Some(DataKey::Never),
             DataKind::Unary { op, input } => Some(DataKey::Unary {
                 op: *op,
                 input: input.addr(),
@@ -317,55 +269,11 @@ impl<'src> DataKind<'src> {
                 mem: mem.addr(),
                 addr: addr.addr(),
             }),
-            DataKind::BranchPhi {
-                merge,
-                when_true,
-                when_false,
-            } => Some(DataKey::BranchPhi {
-                merge: merge.addr(),
-                when_true: when_true.addr(),
-                when_false: when_false.addr(),
-            }),
-            DataKind::LoopPhi {
-                loop_head,
-                entry,
-                backedge,
-            } => Some(DataKey::LoopPhi {
-                loop_head: loop_head.addr(),
-                entry: entry.addr(),
-                backedge: backedge.addr(),
+            DataKind::Phi { phi } => Some(DataKey::Phi {
+                merge: phi.merge.addr(),
+                variants: phi.variants.iter().map(|variant| variant.addr()).collect(),
             }),
             DataKind::Unknown => None,
-        }
-    }
-}
-
-impl<'src> CtrlNode<'src> {
-    pub fn set_backedge(&mut self, backedge: CtrlID<'src>) {
-        match &mut self.kind {
-            CtrlKind::LoopHead {
-                backedge: placeholder_backedge,
-                ..
-            } => *placeholder_backedge = backedge,
-            _ => panic!("tried to set backedge on non-loop-head node"),
-        }
-    }
-
-    pub fn set_condition(&mut self, condition: DataID<'src>) {
-        match &mut self.kind {
-            CtrlKind::LoopHead {
-                condition: placeholder_condition,
-                ..
-            } => *placeholder_condition = condition,
-            _ => panic!("attempted to set loop condition on non-loop-head memory node"),
-        }
-    }
-
-    pub fn set_entry(&mut self, entry: CtrlID<'src>) {
-        match &mut self.kind {
-            CtrlKind::PlaceHolder { ctrl } => *ctrl = entry,
-            CtrlKind::LoopHead { ctrl, .. } => *ctrl = entry,
-            _ => panic!("attempted to set step clause prev on non-step-clause memory node"),
         }
     }
 }
